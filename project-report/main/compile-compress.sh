4fb560788282f554554bab7b11619115eb1b7f29
Marko Lalovic <marko@markolalovic.com>
2021-06-14 14:17:13 +0200

    added project report template

---
+++ project-report/main/compile-compress.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pdflatex -output-directory=../build main.tex
+bibtex ./build/main.aux
+pdflatex -output-directory=../build main.tex
+pdflatex -output-directory=../build main.tex
+
+# compress to screen size (72dpi)
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=../project-report.pdf ../build/main.pdf
