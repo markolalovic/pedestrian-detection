8943aef6744395d96c9c95c5b657796ab28c9976
mbeckmann987 <86412316+mbeckmann987@users.noreply.github.com>
2021-06-25 12:41:03 +0200

    Create results_as_json.py

---
+++ src/results_as_json.py
@@ -0,0 +1,32 @@
+import json
+import numpy as np
+import torch
+
+def results_as_json(model, indices, dataset, device, num_im, threshold = 0.3):
+    infos = []
+    for i in range(0, num_im):
+        idx = indices.index(i)
+        img, anno = dataset[idx]
+        model.eval()
+        with torch.no_grad():
+            prediction = model([img.to(device)])[i]
+
+        preds = prediction['boxes']  # predicted bboxes
+        preds = preds.cpu().data.numpy()  # to numpy array
+        scores = prediction['scores']  # scores of predicted bboxes
+        scores = scores.cpu().data.numpy()
+
+        highs = list(np.where(scores > threshold)[0])
+
+        for high in highs:
+            bb = list(preds[high])
+            sc = scores[high]
+
+            info = {"image_id": idx, #this idx should be the same as the one in the data
+                    "category_id": 1,
+                    "bbox": bb,
+                    "score": sc}
+            infos.append(info)
+
+    f = open("results.json","w")
+    f. write(json.dumps(infos))
