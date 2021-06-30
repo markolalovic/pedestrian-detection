4d49902ce5869ae9b993684f42524fb9d42e556e
mbeckmann987 <86412316+mbeckmann987@users.noreply.github.com>
2021-06-30 15:50:05 +0200

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -6,26 +6,28 @@ TODO: Write a function that converts FasterRCNN results to detections.json file.
 
 """
 
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
+import sys
+sys.path.insert(0, '../src/coco_evaluation_tools/')
+from MR_coco import COCO
+from MR_eval_multisetup import COCOeval
 from contextlib import redirect_stdout
 
 ## set annotations type and paths to files
 annType = 'bbox'
-annFile = '../data/truths.json'
-resFile = '../data/detections.json'
+annFile = '../data/data0.json'
+resFile = '../data/det0.json'
 
 ## testing the evaluation
 if __name__ == "__main__":
-    cocoGt=COCO(annFile)
-    cocoDt = cocoGt.loadRes(resFile)
+    res_file = open("../data/results.txt", "w")
+    for id_setup in range(0, 4):
+        cocoGt = COCO(annFile)
+        cocoDt = cocoGt.loadRes(resFile)
+        imgIds = sorted(cocoGt.getImgIds())
+        cocoEval = COCOeval(cocoGt, cocoDt, annType)
+        cocoEval.params.imgIds = imgIds
+        cocoEval.evaluate(id_setup)
+        cocoEval.accumulate()
+        cocoEval.summarize(id_setup, res_file)
 
-    imgIds = sorted(cocoGt.getImgIds())
-    cocoEval = COCOeval(cocoGt,cocoDt,annType)
-    cocoEval.params.imgIds = imgIds
-    cocoEval.evaluate()
-    cocoEval.accumulate()
-
-    with open('../data/results.txt', 'w') as f:
-        with redirect_stdout(f):
-            cocoEval.summarize()
+    res_file.close()
